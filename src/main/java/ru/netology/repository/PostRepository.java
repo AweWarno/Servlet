2d657c814e6d9c746c7019b67474a28d96ce4156
Alekseu F <Tapo4ek@gmail.com>
2024-07-30 11:55:46 +0300

    init

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,45 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private List<Post> listPost;
+    private Integer idPost = 0;
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        return listPost;
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        long id = post.getId();
+        String content = post.getContent();
+
+        if (id == 0) {
+            post.setId(id + 1);
+            listPost.add(post);
+            return post;
+        }
+
+        if (id != 0) {
+            for (Post list : listPost) {
+                if (list.getId() == id) {
+                    list.setContent(content);
+                    return list;
+                }
+            }
+
+            return new Post(0, "Пост не найден");
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+    }
 }
